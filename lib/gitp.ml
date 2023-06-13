5ee8e80d2d026740501c7441e274eb78ae9a23db
mryykt <avrpic9@gmail.com>
2023-06-13 15:24:12 +0900

    cat command

--- lib/gitp.ml
+++ lib/gitp.ml
@@ -4,38 +4,42 @@ module Search=Git.Search.Make(Digestif.SHA1) (Store)
 
 let get_store ()=Store.v @@ Fpath.v @@ Sys.getcwd ()
 
-let get_latest_commit store=Store.Ref.resolve store Git.Reference.main
+let get_latest_commit store=Store.Ref.resolve store Git.Reference.master
 
-let find_blob store cid filename=Search.find store cid (`Commit (`Path [filename]))
+let find_blob store cid path=Search.find store cid (`Commit (`Path path))
+
+let split_path s=String.split_on_char '/' s |> List.filter ((<>) ".")
+    
+    
 
 let read_blob store blob=Store.read store blob
 
 module Command=struct
   open Cmdliner
 
-  let split_path s=String.split_on_char '/' s |> List.filter ((<>) ".")
-
   module Cat=struct
     let sub=
-      let f file=
+      let f cid file=
         let open Lwt_result.Syntax in
         let* store=get_store() in
-        let* cid=get_latest_commit store in
+        let* cid=if cid="" then get_latest_commit store else Lwt_result.return @@ Digestif.SHA1.of_hex cid in
         let open Lwt.Syntax in
-        let* hash=find_blob store cid file in
+        let* hash=find_blob store cid (split_path file) in
         match hash with
           | Some h->Store.read store h
           | None->Lwt.return (Error (`Not_found cid)) in
       let out=
-        let sub i=function
-          |Git.Value.Blob b->Fmt.string i (Git.Blob.to_string b)
-          |_->Fmt.string  i "Not file" in
+        let sub fmtr=function
+          |Git.Value.Blob b->Fmt.string fmtr (Git.Blob.to_string b)
+          |_->Fmt.string  fmtr "this is not file" in
         Fmt.result ~ok:sub ~error:Store.pp_error in
-      let doc="show file in repository to standard output." in
+      let cid=
+        Arg.(value & opt string "" & info ["c";"commit"] ~docv:"COMMIT" ~doc:"specify commit id") in
       let file=
         Arg.(value & pos 0 string "" & info [] ~docv:"FILE" ~doc:"file name") in
+      let doc="show file in repository to standard output." in
       let info=Cmd.info "cat" ~doc in
-      Cmd.v info Term.(const (fun p->Lwt_main.run Lwt.Infix.(f p>|= out Fmt.stdout)) $ file)
+      Cmd.v info Term.(const (fun c p->Lwt_main.run Lwt.Infix.(f c p>|= out Fmt.stdout)) $ cid $ file)
   end
 
   module Ls=struct
