a162a297dbc0a95b526164abbe5220cf6c49a441
mryykt <avrpic9@gmail.com>
2023-06-24 10:17:02 +0900

    commit

--- _build/default/lib/gitp.ml
+++ _build/default/lib/gitp.ml
@@ -44,11 +44,26 @@ module Command=struct
 
   module Ls=struct
     let sub=
-      let f d=print_endline d in
+      let f cid dir=let open Lwt_result.Syntax in
+        let* store=get_store() in
+        let* cid=if cid="" then get_latest_commit store else Lwt_result.return @@ Digestif.SHA1.of_hex cid in
+        let open Lwt.Syntax in
+        let* hash=find_blob store cid (split_path dir) in
+        let* value=match hash with
+          | Some h->Store.read store h
+          | None->Lwt.return (Error (`Not_found cid)) in
+        match value with
+          |Ok (Git.Value.Tree t)->let open Git.Tree in Git.Tree.to_list t |> List.map (fun e->e.name) |> Lwt.return_ok
+          |_->Lwt.return (Error (`Not_found cid)) in
+      let out=
+        let sub fmtr l=List.iter (fun i->Fmt.string fmtr (i^"\n")) l in
+        Fmt.result ~ok:sub ~error:Store.pp_error in
+      let cid=
+        Arg.(value & opt string "" & info ["c";"commit"] ~docv:"COMMIT" ~doc:"specify commit id") in
       let file=
         Arg.(value & pos 0 string "" & info [] ~docv:"FILE" ~doc:"file name") in
       let info=Cmd.info "ls" in
-      Cmd.v info Term.(const f $ file)
+      Cmd.v info Term.(const (fun c p->Lwt_main.run Lwt.Infix.(f c p>|= out Fmt.stdout)) $ cid $ file)
   end
 
   module Save=struct
